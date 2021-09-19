73d353415e04ed09aa5d0b5d0b7cfb0cb6989013
Pawel Konior <pawel.konior@gmail.com>
2021-09-19 17:04:19 +0200

    add webpack configuration base with images, fonts, scss, modern js

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import "./scss/main.scss";
+
+import Icon from "./images/avatar.png";
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, './src/index.js'),
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                use: {
+                    loader: 'url-loader'
+                }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: 'src/templates/index.html'
+        })
+    ],
+    resolve: {
+        extensions: ['*', '.js']
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        static: path.resolve(__dirname, './dist')
+    }
+}
